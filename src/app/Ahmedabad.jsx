8ee46bf9a96965d4e04353885656676e799a69c6
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-10-27 12:54:58 +0530

    click

--- src/app/Ahmedabad.jsx
+++ src/app/Ahmedabad.jsx
@@ -4,7 +4,7 @@ const Ahmedabad = () => {
     return (
         <div>
             <h3>This is Ahmedabad Data</h3>
-            <a href='https://docs.google.com/spreadsheets/d/1MAprts4ZXyg3uEw3iyXk_sN_KPMsODYpkGTW2NCPNO8/edit#gid=0' target='blank'>Google sheet</a>
+            <a href='https://docs.google.com/spreadsheets/d/1MAprts4ZXyg3uEw3iyXk_sN_KPMsODYpkGTW2NCPNO8/edit#gid=0' target='blank'>Click for Google sheet</a>
         </div>
     )
 }
--- src/app/Mumbai.jsx
+++ src/app/Mumbai.jsx
@@ -4,7 +4,7 @@ const Mumbai = () => {
     return (
         <div>
             <h3>This is Mumbai Data</h3>
-            <a href='https://docs.google.com/spreadsheets/d/1d02u680tSKtCuBPn2M8E6naK_56F8sQqx6J3lCjb-28/edit#gid=0' target='blank'>Google sheet</a>
+            <a href='https://docs.google.com/spreadsheets/d/1d02u680tSKtCuBPn2M8E6naK_56F8sQqx6J3lCjb-28/edit#gid=0' target='blank'>Click for Google sheet</a>
         </div>
     )
 }

a8ebbf827ea8f97543297e29a7f46a4758377140
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-10-27 11:39:43 +0530

    second

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Station Data</h1>
+      <h2>Mumbai</h2>
+      <h2>Ahmedabad</h2>
     </div>
   );
 }
